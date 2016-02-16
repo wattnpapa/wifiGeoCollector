c68915f2f9e9da2e17eed05df737681c63d46180
Johannes Rudolph <johannes.rudolph@gmx.com>
2016-02-16 22:42:30 +0100

    rename vars

--- wifi.js
+++ wifi.js
@@ -4,45 +4,45 @@
 
 
 function Wifi(bssid,rssi,date,position){
-    this._bssid = bssid;
-    this._rssi = rssi;
-    this._date = date;
-    this._position = position;
+    this.bssid = bssid;
+    this.rssi = rssi;
+    this.date = date;
+    this.position = position;
 }
 
 Wifi.prototype = {
     constructor: Wifi,
 
     getBssid: function(){
-        return this._bssid;
+        return this.bssid;
     },
 
     setBssid: function(bssid){
-        this._bssid = bssid;
+        this.bssid = bssid;
     },
 
     getRssi: function(){
-        return this._rssi;
+        return this.rssi;
     },
 
     setRssi: function(rssi){
-        this._rssi = rssi;
+        this.rssi = rssi;
     },
 
     getDate: function(){
-        return this._date;
+        return this.date;
     },
 
     setDate: function(date){
-        this._date = date
+        this.date = date
     },
 
     getPosition: function(){
-        return this._position;
+        return this.position;
     },
 
     setPosition: function(position){
-        this._position = position;
+        this.position = position;
     }
 };
 
