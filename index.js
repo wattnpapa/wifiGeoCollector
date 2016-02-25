956638ccdbdfc25ec26778780ef2de61af63938b
Johannes Rudolph <johannes.rudolph@gmx.com>
2016-02-25 22:57:55 +0000

    Add a lot of stuff: Set Date/Time from GPS Set Position from GPS add Interval for scanning

--- index.js
+++ index.js
@@ -1,25 +1,48 @@
 // Modules
 var wifiscanner = require('node-wifiscanner');
 var Wifi = require('./wifi.js');
+var serialgps = require('super-duper-serial-gps-system');
+var gps = new serialgps('/dev/ttyACM0',9600);
+var cmd = require("cmd-exec").init();
 
 // Variables
 var position =  {lat:0, lng:0};
-var start = new Date();
-scanWifis();
+
+//Set Date Time by GPS
+gps.on('nav-info', function(data) {
+    var time = Math.round(data.timestamp)+"";
+    time = time.substr(0,2) + ":" + time.substr(2,2) + ":" + time.substr(4,2);
+    var date = "20" + data.date.substr(4,2) + "-"+ data.date.substr(2,2) + "-" + data.date.substr(0,2);
+    cmd.exec('date -s "'+date+' '+time+'"');
+});
+
+//Update Position
+gps.on('position', function(data) {
+    position.lat = Math.round(data.latitude*1000000)/1000000;
+    position.lng = Math.round(data.longitude*1000000)/1000000;
+});
+
+setInterval(scanWifis,10000);
 
 function scanWifis(){
-    wifiscanner.scan(function(err, data){
-        if (err) {
-            console.log("Error : " + err);
-            return;
-        }
+    if(position.lat != 0 && position.lng != 0){
+	console.log("Start Scan");
+        var start = new Date();
+    	wifiscanner.scan(function(err, data){
+            if (err) {
+                console.log("Error : " + err);
+                return;
+            }
 
-        for(var i = 0; i < data.length ; i++){
-            var wifi = new Wifi(data[i].mac,data[i].signal_level, new Date(),position);
-            console.log(wifi);
-        }
+            for(var i = 0; i < data.length ; i++){
+                var wifi = new Wifi(data[i].mac,data[i].signal_level, new Date(),position);
+                console.log(wifi);
+            }
 
         console.log(start);
         console.log(new Date());
     });
+    }
 }
+
+
